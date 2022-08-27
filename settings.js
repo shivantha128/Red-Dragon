8f061faa115113a5fa03df3f1aed4333425a57b7
shivantha128 <112193134+shivantha128@users.noreply.github.com>
2022-08-27 09:45:07 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,7 +29,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+94785776286'] // owner number
+global.vcardowner = ['+94778993606'] // owner number
 global.ownername = "😎w.A.shivantha❯❯" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
@@ -47,8 +47,8 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+94785776286']
-global.ownertag = '+94785776286' //your tag number
+global.owner = ['+94778993606']
+global.ownertag = '+94778993606' //your tag number
 global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
@@ -56,7 +56,7 @@ global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ shiva ]' //
 
 //database
-global.premium = ['+94785776286'] //ur premium numbers
+global.premium = ['+94778993606'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
